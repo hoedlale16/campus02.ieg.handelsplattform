c9b6f971ce8435b7c9cc4a611761a2a0363cebbb
Alexander H <alexander.hoedl@edu.fh-joanneum.at>
2019-11-01 11:28:21 +0100

    A2T3 - Logging and Resiliance

---
+++ BlackFriday/ServiceClients/SimpleCreditCartServiceClient.cs
@@ -0,0 +1,58 @@
+﻿using BlackFriday.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackFriday.ServiceClients
+{
+    public class SimpleCreditCartServiceClient
+    {
+        private readonly HttpClient _client;
+
+        private readonly string[] urls =
+        {
+            "bla","bla"
+        };
+
+        public SimpleCreditCartServiceClient()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri($"http://iegGr8easycreditcardservice.azurewebsites.net/")
+            };
+        }
+
+        public async Task<bool> PostCreditcartTransaction(CreditcardTransaction creditCardTransaction)
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(creditCardTransaction), Encoding.UTF8);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await _client.PostAsync(new Uri($"/api/CreditcardTransactions"), httpContent);
+            return (response.IsSuccessStatusCode) ?  true :  false;
+        }
+
+
+        public async Task<List<string>> GetPaymentMethods()
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _client.GetAsync(new Uri($"/api/AcceptedCreditCards"));
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<List<string>>();
+            }
+
+            return null;
+        }
+    }
+}
